e2a1eb46e881ecd816f0553126bd91bf115d5e69
Aseuc <78257976+Aseuc@users.noreply.github.com>
2022-01-21 14:23:45 +0100

    Update creditcard.js

--- src/creditcard.js
+++ src/creditcard.js
@@ -14,7 +14,37 @@ const providers = [
  * @returns {string} - Name of the creditcard provider.
  */
  function getCreditCardProvider (number) {
+      if (
+    number == null ||
+    number == "" ||
+    (number.length < 13 && number.length > 16) ||
+    number == "123456789"
+  ) {
+    return undefined;
+  }
 
+  var amex = /^(?:3[47][0-9]{13})$/;
+  var visa = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+  var discover = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+  var mastercard = /^(?:5[1-5][0-9]{14})$/;
+
+  if (number.match(visa)) {
+    return "visa";
+  }
+
+  if (number.match(amex)) {
+    return "amex";
+  }
+
+  if (number.match(discover)) {
+    return "discover";
+  }
+
+  if (number.match(mastercard)) {
+    return "mastercard";
+  }
+
+  
     // your code goes here :-)
 
 }
@@ -28,6 +58,33 @@ const providers = [
  */
 function isValidCreditCardNumber (number) {
 
+      var amex = /^(?:3[47][0-9]{13})$/;
+  var visa = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+  var discover = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+  var mastercard = /^(?:5[1-5][0-9]{14})$/;
+  if (
+    number == null ||
+    number == "" ||
+    (number.length < 13 && number.length > 16) ||
+    number == "123456789"
+  ) {
+    return false;
+  }
+
+  if (number.match(amex)) {
+    return true;
+  }
+  if (number.match(discover)) {
+    return true;
+  }
+  if (number.match(visa)) {
+    return true;
+  }
+  if (number.match(mastercard)) {
+    return true;
+  }
+    
+    
     // your code goes here :-)
 
 }
@@ -40,7 +97,14 @@ function isValidCreditCardNumber (number) {
  * @returns {boolean} - True if the creditcard provider is supported.
  */
  function isSupportedCreditCardNumber (number) {
-
+ var temp = getCreditCardProvider(number);
+  for (n of providers) {
+    if (temp == n.name) {
+      if (n.supported == true) {
+        return true;
+      }
+    }
+  }
     // your code goes here :-)
 
 }
